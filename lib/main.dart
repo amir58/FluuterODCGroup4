796341558de128e0051f1d494bde6ae1e14a3e30
amir58 <devamir11@gmail.com>
2022-08-18 10:23:33 +0200

    Add login with google

--- lib/main.dart
+++ lib/main.dart
@@ -110,6 +110,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void loginWithGoogle(){
+    // some code
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
