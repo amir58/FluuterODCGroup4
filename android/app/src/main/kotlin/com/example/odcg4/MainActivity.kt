43e0ee367a49239ed0f603abccd5a21769769c0f
amir58 <devamir11@gmail.com>
2022-08-18 10:13:05 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/odcg4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.odcg4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/NewsResponse.dart
@@ -0,0 +1,147 @@
+/// status : "ok"
+/// totalResults : 11
+/// articles : [{"source":{"id":null,"name":"Newsmisr.info"},"author":null,"title":"لو عايز تبني.. أسعار الحديد والأسمنت اليوم الثلاثاء 16 أغسطس 2022 في سوق البناء - نيوز مصر","description":"شهدت أسعار مواد البناء المكونة من الحديد والأسمنت خلال بداية تعاملات اليوم الثلاثاء الموافق 16 أغسطس 2022 في كافة المصانع والشركات في الأسواق داخل مصر،","url":"https://www.newsmisr.info/2022/08/16/%d9%84%d9%88-%d8%b9%d8%a7%d9%8a%d8%b2-%d8%aa%d8%a8%d9%86%d9%8a-%d8%a3%d8%b3%d8%b9%d8%a7%d8%b1-%d8%a7%d9%84%d8%ad%d8%af%d9%8a%d8%af-%d9%88%d8%a7%d9%84%d8%a3%d8%b3%d9%85%d9%86%d8%aa-%d8%a7%d9%84%d9%8a/","urlToImage":"https://www.newsmisr.info/wp-content/uploads/2022/08/IMG_٢٠٢٢٠٨١٦_٠٥٥٠١٧.jpg","publishedAt":"2022-08-16T06:10:19Z","content":null},{"source":{"id":null,"name":"Muhtwaplus.com"},"author":"جابر السويفي","title":"أرخص سيارة زيرو موديل 2023 في مصر..\"سعر يبهرك\" - محتوى بلس","description":"يبحث الكثيرون من الراغبين في شراء سيارة عن أرخص سيارة زيرو موديل 2023 في مصر، من أجل شرائها لمساعدتهم في حياتهم اليومية، بالإضافة إلى التخلص من زحمة المواصلات. ماهي أرخص سيارة زيرو موديل 2023 في مصر تعتبر السيارة لادا جرانتا من أرخص السيارات موديل 2023 في الس…","url":"https://muhtwaplus.com/271175/2022/08/16/%d8%a3%d8%b1%d8%ae%d8%b5-%d8%b3%d9%8a%d8%a7%d8%b1%d8%a9-%d8%b2%d9%8a%d8%b1%d9%88-%d9%85%d9%88%d8%af%d9%8a%d9%84-2023-%d9%81%d9%8a-%d9%85%d8%b5%d8%b1-%d8%b3%d8%b9%d8%b1-%d9%8a%d8%a8%d9%87%d8%b1%d9%83/","urlToImage":"https://cdn.muhtwaplus.com/wp-content/uploads/95.jpg","publishedAt":"2022-08-15T22:58:24Z","content":null},{"source":{"id":null,"name":"Elaosboa.com"},"author":"الأسبوع","title":"بكام النهارده.. سعر الذهب في مصر اليوم الثلاثاء 16 أغسطس - الأسبوع","description":"في بداية تعاملات اليوم الثلاثاء، تستمر حالة التذبذب في سعر الذهب ليفقد 15 جنيه فى تعاملات أمس ويعود لاستعادتها 10 جنيهات و5 جنيهات خلال التعاملات…","url":"https://www.elaosboa.com/405144/","urlToImage":"https://www.elaosboa.com/wp-content/uploads/2021/12/elaosboa76021.jpg","publishedAt":"2022-08-15T22:29:08Z","content":"<ol><li></li><li></li></ol> 15 10 5 21 1063 .\r\n18 911.25 .\r\n 21 1063 .\r\n 24 1214.75 .\r\n 14 708.75 .\r\n 8504 .\r\n.\r\n 1.2 1780.40 () 1750 .\r\n 1.06 1796.30 .\r\n 0.4 .\r\n30 65 .\r\n 7% 10% \"31.1 \" .\r\n ."},{"source":{"id":"google-news","name":"Google News"},"author":null,"title":"سعر ومواصفات سيارة جيلي باندينو الشبابية.. طلع اللي تحت البلاطة واشتريها - عرب فايف","description":null,"url":"https://news.google.com/__i/rss/rd/articles/CBMingNodHRwczovL3d3dy5hcmFiZml2ZS5uZXdzLzk4NTk5LzIwMjIvMDgvMTUvJUQ4JUIzJUQ4JUI5JUQ4JUIxLSVEOSU4OCVEOSU4NSVEOSU4OCVEOCVBNyVEOCVCNSVEOSU4MSVEOCVBNyVEOCVBQS0lRDglQjMlRDklOEElRDglQTclRDglQjElRDglQTktJUQ4JUFDJUQ5JThBJUQ5JTg0JUQ5JThBLSVEOCVBOCVEOCVBNyVEOSU4NiVEOCVBRiVEOSU4QSVEOSU4NiVEOSU4OC0lRDglQTclRDklODQlRDglQjQlRDglQTglRDglQTclRDglQTglRDklOEElRDglQTktJUQ4JUI3JUQ5JTg0JUQ4JUI5LSVEOCVBNyVEOSU4NCVEOSU4NCVEOSU4QS0lRDglQUElRDglQUQlRDglQUEtJUQ4JUE3JUQ5JTg0JUQ4JUE4JUQ5JTg0JUQ4JUE3JUQ4JUI3JUQ4JUE5LSVEOSU4OCVEOCVBNyVEOCVCNCVEOCVBQSVEOCVCMSVEOSU4QSVEOSU4NyVEOCVBNy_SAQA?oc=5","urlToImage":null,"publishedAt":"2022-08-15T16:34:33Z","content":null},{"source":{"id":null,"name":"Investing.com"},"author":"Investing.com","title":"عاجل: سقوط جماعي للذهب ونفط وأسهم وكريبتو.. إلا واحد بواسطة Investing.com - السعودية Investing.com","description":"عاجل:  سقوط جماعي للذهب ونفط وأسهم وكريبتو.. إلا واحد","url":"https://sa.investing.com/news/economy/article-2307546","urlToImage":"https://i-invdn-com.investing.com/news/LYNXNPEC0K08U_L.jpg","publishedAt":"2022-08-15T14:51:00Z","content":"%USER_NAME%\r\n %USER_NAME% Investing.com."},{"source":{"id":"google-news","name":"Google News"},"author":null,"title":"انخفاض سعر الدولار اليوم في البنك المركزي مقابل الجنيه المصري - Al Masry Al Youm - المصري اليوم","description":null,"url":"https://news.google.com/__i/rss/rd/articles/CBMiMmh0dHBzOi8vd3d3LmFsbWFzcnlhbHlvdW0uY29tL25ld3MvZGV0YWlscy8yNjY1OTcx0gEA?oc=5","urlToImage":null,"publishedAt":"2022-08-15T13:23:00Z","content":null},{"source":{"id":null,"name":"Masrawy.com"},"author":"مصراوي","title":"في 10 بنوك.. رسوم السحب والاستعلام عن الرصيد عبر أي ماكينة ATM - Masrawy - مصراوي","description":"في 10 بنوك رسوم السحب والاستعلام عن الرصيد عبر أي ماكينة atm | مصراوى","url":"https://www.masrawy.com/news/news-banking/details/2022/8/15/2275344/في-10-بنوك-رسوم-السحب-والاستعلام-عن-الرصيد-عبر-أي-ماكينة-atm-","urlToImage":"https://media.gemini.media/img/large/2020/10/25/2020_10_25_12_37_23_760.jpg","publishedAt":"2022-08-15T13:00:00Z","content":"- :\r\n (ATM) .\r\n 27 2020 2022 .\r\n 5 .\r\n 4 .\r\n ATM 10 :\r\n : 5 1.5 .\r\n : 5 .\r\n : 5 .\r\n : 5 .\r\n HSBC: 5 .\r\n : 5 3 .\r\n QNB: 5 .\r\n : 5 .\r\n : 5 4 .\r\n : 5 ."},{"source":{"id":null,"name":"Nl7za.com"},"author":"احمد المصري","title":"كسب 5000 جنية شهريا من شهادات ادخار بنك مصر والبنك الاهلي المصري استثمار فلوسك في الشهادات - جريدة لحظات نيوز","description":"شهادات ادخار بنك مصر والبنك الاهلي المصري استثمار فلوسك في الشهادات واصل العديد من الآباء البحث في محرك Google الشهير للحصول على هدايا لأطفالهم مع الإعلان عن درجات المدرسة الثانوية يوم السبت. الشهادة هي شهادة استثمار، مع تقدم العديد من البنوك العاملة","url":"https://nl7za.com/%d8%a7%d9%84%d8%a7%d9%82%d8%aa%d8%b5%d8%a7%d8%af/226982/%d9%83%d8%b3%d8%a8-5000-%d8%ac%d9%86%d9%8a%d8%a9-%d8%b4%d9%87%d8%b1%d9%8a%d8%a7-%d9%85%d9%86-%d8%b4%d9%87%d8%a7%d8%af%d8%a7%d8%aa-%d8%a7%d8%af%d8%ae%d8%a7%d8%b1-%d8%a8%d9%86%d9%83-%d9%85%d8%b5%d8%b1/","urlToImage":"https://nl7za.com/wp-content/uploads/2022/01/13-1024x1024.jpg","publishedAt":"2022-08-15T12:16:29Z","content":"Google . . 3 1000 14 . 12.25.000 6 .\r\n 2022\r\n 13 3 500 2500 . + ) .\r\n18 59 . . 50000 250 . 60 .\r\n14% \r\n 14 :\r\n 1- \r\n<ul><li>–     18%.</li><li>– 14% 14% .</li><li>– 14% .</li><li>– 3 1000 .</li></ul>… [+207 chars]"},{"source":{"id":null,"name":"Investing.com"},"author":"Investing.com","title":"عاجل: النفط يسقط 5% ويقترب من قاع 8 أشهر.. ضربة مفاجأة من طهران بواسطة Investing.com - السعودية Investing.com","description":"عاجل: النفط يسقط 5% ويقترب من قاع 8 أشهر.. ضربة مفاجأة من طهران","url":"https://sa.investing.com/news/commodities-news/article-2307437","urlToImage":"https://i-invdn-com.investing.com/news/LYNXMPEBB70BB_L.jpg","publishedAt":"2022-08-15T11:37:00Z","content":"Investing.com - .\r\n 1% 4% 90 (TADAWUL:2222) .\r\n 5 .\r\n: .. \r\n.\r\n : \" \".\r\n: .. \r\n .\r\n : \" \".\r\n : \" \".\r\n: .. \r\n3.8 .\r\n (LON:BP) 16.1% 540 2021 .\r\n: .. \r\n 3.17 2.73 2020 .\r\n 40% .\r\n: .. \r\n2022 8 2022.\r\n … [+39 chars]"},{"source":{"id":"google-news","name":"Google News"},"author":null,"title":"تحديث منتصف اليوم للذهب 15-08-2022 - FxNewsToday","description":null,"url":"https://news.google.com/__i/rss/rd/articles/CBMiwgFodHRwczovL3d3dy5meG5ld3N0b2RheS5hZS9jb21tb2RpdGllcy9nb2xkLWFuYWx5c2lzLyVEOCVBQSVEOCVBRCVEOCVBRiVEOSU4QSVEOCVBQi0lRDklODUlRDklODYlRDglQUElRDglQjUlRDklODEtJUQ4JUE3JUQ5JTg0JUQ5JThBJUQ5JTg4JUQ5JTg1LSVEOSU4NCVEOSU4NCVEOCVCMCVEOSU4NyVEOCVBOC0xNS0wOC0yMDIyLTIxNDM3MtIBAA?oc=5","urlToImage":null,"publishedAt":"2022-08-15T08:52:27Z","content":null},{"source":{"id":null,"name":"Alarabiya.net"},"author":"العربية","title":"بيتكوين تتجاوز 25000 دولار لأول مرة منذ يونيو.. هل انتهى شتاء التشفير؟ - العربية","description":"تجاوزت عملة بيتكوين لفترة وجيزة 25000 دولار لأول مرة منذ منتصف يونيو، حيث استمر الزخم بسبب بيانات التضخم الأميركية التي جاءت أقل من المتوقع والتقدم نحو ترق","url":"https://www.alarabiya.net/aswaq/special-stories/2022/08/14/%D8%A8%D9%8A%D8%AA%D9%83%D9%88%D9%8A%D9%86-%D8%AA%D8%AA%D8%AC%D8%A7%D9%88%D8%B2-25000-%D8%AF%D9%88%D9%84%D8%A7%D8%B1-%D9%84%D8%A3%D9%88%D9%84-%D9%85%D8%B1%D8%A9-%D9%85%D9%86%D8%B0-%D9%8A%D9%88%D9%86%D9%8A%D9%88-%D9%87%D9%84-%D8%A7%D9%86%D8%AA%D9%87%D9%89-%D8%B4%D8%AA%D8%A7%D8%A1-%D8%A7%D9%84%D8%AA%D8%B4%D9%81%D9%8A%D8%B1%D8%9F","urlToImage":"https://vid.alarabiya.net/images/2022/07/11/97ffe30e-10e8-42b1-b53e-efea7e818ec8/97ffe30e-10e8-42b1-b53e-efea7e818ec8_16x9_600x338.jpg","publishedAt":"2022-08-14T10:35:00Z","content":"25000 .\r\n 2.2% 25.031 13 24750 .\r\n 2% 2030.50 2000 31 \"\" \"\".\r\n Fundstrat : \" \". \" \".\r\n 50%. 100 - .\r\n : \" \" - .\r\n ."}]
+
+class NewsResponse {
+  NewsResponse({
+      String? status, 
+      int? totalResults, 
+      List<Articles>? articles,}){
+    _status = status;
+    _totalResults = totalResults;
+    _articles = articles;
+}
+
+  NewsResponse.fromJson(dynamic json) {
+    _status = json['status'];
+    _totalResults = json['totalResults'];
+    if (json['articles'] != null) {
+      _articles = [];
+      json['articles'].forEach((v) {
+        _articles?.add(Articles.fromJson(v));
+      });
+    }
+  }
+  String? _status;
+  int? _totalResults;
+  List<Articles>? _articles;
+
+  String? get status => _status;
+  int? get totalResults => _totalResults;
+  List<Articles>? get articles => _articles;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = _status;
+    map['totalResults'] = _totalResults;
+    if (_articles != null) {
+      map['articles'] = _articles?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
+
+/// source : {"id":null,"name":"Newsmisr.info"}
+/// author : null
+/// title : "لو عايز تبني.. أسعار الحديد والأسمنت اليوم الثلاثاء 16 أغسطس 2022 في سوق البناء - نيوز مصر"
+/// description : "شهدت أسعار مواد البناء المكونة من الحديد والأسمنت خلال بداية تعاملات اليوم الثلاثاء الموافق 16 أغسطس 2022 في كافة المصانع والشركات في الأسواق داخل مصر،"
+/// url : "https://www.newsmisr.info/2022/08/16/%d9%84%d9%88-%d8%b9%d8%a7%d9%8a%d8%b2-%d8%aa%d8%a8%d9%86%d9%8a-%d8%a3%d8%b3%d8%b9%d8%a7%d8%b1-%d8%a7%d9%84%d8%ad%d8%af%d9%8a%d8%af-%d9%88%d8%a7%d9%84%d8%a3%d8%b3%d9%85%d9%86%d8%aa-%d8%a7%d9%84%d9%8a/"
+/// urlToImage : "https://www.newsmisr.info/wp-content/uploads/2022/08/IMG_٢٠٢٢٠٨١٦_٠٥٥٠١٧.jpg"
+/// publishedAt : "2022-08-16T06:10:19Z"
+/// content : null
+
+class Articles {
+  Articles({
+      Source? source, 
+      dynamic author, 
+      String? title, 
+      String? description, 
+      String? url, 
+      String? urlToImage, 
+      String? publishedAt, 
+      dynamic content,}){
+    _source = source;
+    _author = author;
+    _title = title;
+    _description = description;
+    _url = url;
+    _urlToImage = urlToImage;
+    _publishedAt = publishedAt;
+    _content = content;
+}
+
+  Articles.fromJson(dynamic json) {
+    _source = json['source'] != null ? Source.fromJson(json['source']) : null;
+    _author = json['author'];
+    _title = json['title'];
+    _description = json['description'];
+    _url = json['url'];
+    _urlToImage = json['urlToImage'];
+    _publishedAt = json['publishedAt'];
+    _content = json['content'];
+  }
+  Source? _source;
+  dynamic _author;
+  String? _title;
+  String? _description;
+  String? _url;
+  String? _urlToImage;
+  String? _publishedAt;
+  dynamic _content;
+
+  Source? get source => _source;
+  dynamic get author => _author;
+  String? get title => _title;
+  String? get description => _description;
+  String? get url => _url;
+  String? get urlToImage => _urlToImage;
+  String? get publishedAt => _publishedAt;
+  dynamic get content => _content;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (_source != null) {
+      map['source'] = _source?.toJson();
+    }
+    map['author'] = _author;
+    map['title'] = _title;
+    map['description'] = _description;
+    map['url'] = _url;
+    map['urlToImage'] = _urlToImage;
+    map['publishedAt'] = _publishedAt;
+    map['content'] = _content;
+    return map;
+  }
+
+}
+
+/// id : null
+/// name : "Newsmisr.info"
+
+class Source {
+  Source({
+      dynamic id, 
+      String? name,}){
+    _id = id;
+    _name = name;
+}
+
+  Source.fromJson(dynamic json) {
+    _id = json['id'];
+    _name = json['name'];
+  }
+  dynamic _id;
+  String? _name;
+
+  dynamic get id => _id;
+  String? get name => _name;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = _id;
+    map['name'] = _name;
+    return map;
+  }
+
+}
